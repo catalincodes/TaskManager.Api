a8d187977964496866d1008e300ccafb5f3304f2
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2026-01-28 19:29:04 -0500

    Update UI to fetch data from the taskService

--- task-manager-ui/src/App.tsx
+++ task-manager-ui/src/App.tsx
@@ -1,7 +1,47 @@
+import {useEffect, useState} from "react";
+import {getTasks} from "./services/taskService";
+import type {ToDoTask} from "./types/ToDoTask.ts";
+
 function App() {
+    const [tasks, setTasks] = useState<ToDoTask[]>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>("");
+
+    useEffect(() => {
+        const fetchTasks = async() => {
+            try{
+                const result = await getTasks();
+                setTasks(result);
+            }
+            catch(error)
+            {
+                if (error instanceof Error)
+                    setError(error.message);
+                else 
+                    setError("Unexpected error");
+                setTasks([]);
+            }
+            finally
+            {
+                setIsLoading(false);
+            }
+        }
+        
+        void fetchTasks();
+    }, []);
+    
+    if (isLoading) 
+        return <p>Loading...</p>;
+    
+    if (error) 
+        return <p>Error: {error}</p>;
+    
+    const listTasks = tasks.map(task => <li key={task.id}>{task.title}</li>);
+    
   return (
     <>
         <h1>Task Manager</h1>
+        <ul>{listTasks}</ul>
     </>
   )
 }
