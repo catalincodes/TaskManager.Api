855965f885624d6f07f03f447bad81821bda3ad3
Catalin Codes <12548925+catalincodes@users.noreply.github.com>
2026-01-28 19:34:46 -0500

    Get data directly from the backend

--- task-manager-ui/src/services/taskService.ts
+++ task-manager-ui/src/services/taskService.ts
@@ -1,25 +1,6 @@
 import type {ToDoTask} from "../types/ToDoTask.ts"
 import type {TaskDTO} from "../types/TaskDTO.ts";
 
-const mockData: TaskDTO[] = [
-    {
-        "id": 1,
-        "title": "Buy milk",
-        "description": "Buy milk from the supermarket",
-        "dueDate": "2026-02-15",
-        "status": "inProgress",
-        "priority": "low"
-    },
-    {
-        "id": 2,
-        "title": "Buy eggs",
-        "description": "Buy eggs from the supermarket",
-        "dueDate": "2026-02-31",
-        "status": "toDo",
-        "priority": "low"
-    }
-]
-
 export function mapTaskDTOsToToDoTasks(taskDTOs: TaskDTO[]): ToDoTask[] {
     return taskDTOs.map(taskDTO => { 
         return {
@@ -34,10 +15,13 @@ export function mapTaskDTOsToToDoTasks(taskDTOs: TaskDTO[]): ToDoTask[] {
 }
 
 export const getTasks = async ():Promise<ToDoTask[]> => {
-    //Simulate get data from backend
-    const dataFromApi: TaskDTO[] = await new Promise<TaskDTO[]>((resolve) =>
-        setTimeout(() => resolve(mockData), 1000));
-
+    const response = await fetch('/api/tasks');
+    
+    if (!response.ok) 
+        throw new Error('Error fetching tasks');
+    
+    const dataFromApi: TaskDTO[] = await response.json();
+    
     //Return mapped ToDoTasks
     return mapTaskDTOsToToDoTasks(dataFromApi);
 }
